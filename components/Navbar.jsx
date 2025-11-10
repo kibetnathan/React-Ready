3f306baf95769c7a9ae99138c7d0aef9304197bb
kibetnathan <kibetnat@gmail.com>
2025-11-10 15:41:44 +0300

    Add file structure and routing

---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+// src/components/Navbar.jsx
+import { Link, NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="flex gap-4 p-4 bg-gray-200">
+      <NavLink
+        to="/"
+        
+        className={({ isActive }) =>
+          isActive ? "text-blue-600 font-bold" : "text-gray-700"
+        }
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to="/docs"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600 font-bold" : "text-gray-700"
+        }
+      >
+        Docs
+      </NavLink>
+    </nav>
+  );
+}
