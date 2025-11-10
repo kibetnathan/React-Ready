6319a2186e99177ee7fccb5d500495c72263d2fe
kibetnathan <kibetnat@gmail.com>
2025-11-10 16:42:17 +0300

    Make Home page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-neutral-700 text-white pt-12 pb-8">
+    <footer className="bg-blue-900 text-white pt-12 pb-8">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
           {/* Company Info */}
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ReactReadyLogo from "../assets/ReactReadyLogo.png"
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <section className="mx-auto max-w-[1440px] min-w-[280px] py-8 px-4 sm:px-8 md:px-10 lg:px-20">
+      <div className="w-full rounded-[50px] sm:rounded-[60px] md:rounded-[70px] lg:rounded-[80px] bg-[#1e40af] text-white flex flex-col lg:flex-row items-center py-6 sm:py-9 md:py-11 lg:py-14 px-2 sm:px-4 md:px-8 xl:px-16">
+        <img
+          className="w-full lg:w-1/2 xl:w-full max-w-[550px] lg:order-2 rounded-[50%]"
+          src={ReactReadyLogo}
+          alt="Modern design illustration"
+        />
+
+        <div className="text-center md:text-left">
+          <h1 className="text-4xl leading-[48px] md:text-5xl md:leading-[58px] lg:text-[50px] lg:leading-[70px] font-bold mb-6 md:mb-12">
+            Elevate Your React Tools with React Ready
+          </h1>
+
+          <span className="text-xl leading-[34px] underline font-semibold sm:text-[24px] mb-3 mt-5 block">
+            Learn and Create from top notch Examples
+          </span>
+
+          <p className="text-xl leading-[27px] font-normal sm:text-[24px] mb-8 md:mb-12">
+            Explore tutorials and templates to shift your dev process to the next gear. 
+          </p>
+        <Link to='/docs'>
+          <button className="w-full flex items-center justify-between outline-gray-600 max-w-[350px] text-xl font-bold sm:text-lg rounded-[38px] bg-white text-[#1e40af] py-4 px-6 sm:px-9 hover:bg-gray-100 transition-all">
+            <span>Get Started</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              height="20"
+              width="20"
+              viewBox="0 0 330 330"
+            >
+              <path d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255  s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0  c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"></path>
+            </svg>
+          </button>
+          </Link>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
